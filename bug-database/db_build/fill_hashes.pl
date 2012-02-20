73fc207cdd52ee23f45bd3e75eb03f5e63efd95c
Jiri Slaby <jirislaby@gmail.com>
2012-02-20 18:23:51 +0100

    fill_hashes: add
    Used to fill the hashes into the database.
    
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ bug-database/db_build/fill_hashes.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+use strict;
+use Helper;
+use DBI;
+
+die "wrong commandline. should be $0 dest.db" if @ARGV < 1;
+
+my $out = shift @ARGV;
+
+my $hlp = Helper->new($out);
+my $dbh = $hlp->get_dbh;
+
+my $data = $dbh->prepare("UPDATE error SET loc_file_hash = ? " .
+		"WHERE project = ? AND project_version = ? AND loc_file = ?") ||
+		die "cannot UPDATE error: " . DBI::errstr;
+
+while (<>) {
+	chomp;
+	die "unknown input: $_" unless (/^(.*);([0-9a-f]{40})$/);
+	my ($file, $hash) = ($1, $2);
+	$data->execute($hash, 1, "2.6.28", $file) ||
+			die "cannot UPDATE error: " . DBI::errstr;
+}
+
+$dbh->commit;
+
+0;
